d3d625a39d5084c6099851ab537ceae9fb442f6a
Saurav Hathi <61316762+sauravhathi@users.noreply.github.com>
2023-03-22 20:54:00 +0530

    update footer link

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,17 +1,17 @@
 const Footer = () => {
     return (
         <footer className="p-4 bg-slate-900 text-slate-50 w-full">
-                <p className="text-center">
-                    Made with ❤️ by{" "}
-                    <a
-                        href="https://twitter.com/sauravhathi"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="text-blue-500"
-                    >
-                        @sauravhathi
-                    </a>
-                </p>
+            <p className="text-center">
+                Made with ❤️ by{" "}
+                <a
+                    href="https://github.com/sauravhathi"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-blue-500"
+                >
+                    @sauravhathi
+                </a>
+            </p>
         </footer>
     );
 };
